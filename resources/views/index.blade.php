575e8daee1faaad50fab46bc13ed554c372117ee
Saurav-VIT <sauravp@vitinfotech.com>
2025-01-12 12:31:40 +0530

    Do not dry PHP

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,10 +7,14 @@
 
     <div>There are a lot of tasks present</div>
     @forelse ($tasks as $task)
-       <a href="{{ route('tasks.show', ['id' => $task->id ])}}">{{$task -> title}}</a>
+       <a href="{{ route('tasks.show', ['task' => $task->id ])}}">{{$task -> title}}</a>
        <hr>
     @empty
        <div>There are not much tasks</div>
     @endforelse
 
+
+    @if($tasks->count())
+      <nav>{{ $tasks->links()}}</nav>
+    @endif
 @endsection
